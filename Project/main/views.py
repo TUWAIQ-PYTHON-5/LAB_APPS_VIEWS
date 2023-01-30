df62cdb4b918052afa2bbe4442c9bf30be3dbe0c
Anfal Ali <name@gmail.com>
2023-01-31 00:33:59 +0300

    LAB_APPS_VIEWS

---
+++ Project/main/views.py
@@ -0,0 +1,11 @@
+
+from django.shortcuts import render
+from django.http import HttpRequest,HttpResponse
+# Create your views here.
+
+def index (request:HttpRequest):
+    result="Hello World, This is my new HOME !"
+    return HttpResponse(result)
+def index1(request:HttpRequest):
+    result=" A simple paragraph about the website. "
+    return HttpResponse(result)
