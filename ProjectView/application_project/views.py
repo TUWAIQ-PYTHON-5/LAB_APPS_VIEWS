e4bdf5f20aa62afa68503d1be7b543ce89913070
asrar <asrar.rifae@hotmail.com>
2023-01-30 15:06:11 +0300

    Solv lab

---
+++ ProjectView/application_project/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
+# Create your views here.
+
+def home(request : HttpRequest):
+
+    result = "Hello World, This is my new HOME !"
+
+    return HttpResponse(result)
+
+
+
+def about(request : HttpRequest):
+
+    result = "About"
+
+    return HttpResponse(result)
